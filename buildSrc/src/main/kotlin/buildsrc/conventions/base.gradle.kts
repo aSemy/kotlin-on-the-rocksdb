9df92709c41b5aa6ead214c949df3643097280fc
Adam <897017+aSemy@users.noreply.github.com>
2023-01-07 23:18:13 +0100

    increase Gradle test log output

--- buildSrc/src/main/kotlin/buildsrc/conventions/base.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/conventions/base.gradle.kts
@@ -1,5 +1,8 @@
 package buildsrc.conventions
 
+import java.time.Duration
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     base
 }
@@ -17,8 +20,21 @@ tasks.withType<AbstractArchiveTask>().configureEach {
     isReproducibleFileOrder = true
 }
 
-tasks.withType<AbstractArchiveTask>().configureEach {
-    // https://docs.gradle.org/current/userguide/working_with_files.html#sec:reproducible_archives
-    isPreserveFileTimestamps = false
-    isReproducibleFileOrder = true
+tasks.withType<AbstractTestTask>().configureEach {
+    timeout.set(Duration.ofMinutes(10))
+
+    testLogging {
+        showCauses = true
+        showExceptions = true
+        showStackTraces = true
+        showStandardStreams = true
+        events(
+            TestLogEvent.PASSED,
+            TestLogEvent.FAILED,
+            TestLogEvent.SKIPPED,
+             TestLogEvent.STARTED,
+             TestLogEvent.STANDARD_ERROR,
+             TestLogEvent.STANDARD_OUT,
+        )
+    }
 }
