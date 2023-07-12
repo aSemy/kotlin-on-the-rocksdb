da2ada03f6b57068e9b85290c3b656124dfd01d0
Adam <897017+aSemy@users.noreply.github.com>
2023-07-12 13:49:05 +0200

    replace deprecated `getTimeMillis()` in `testUtils.kt#tempDir()`

--- modules/rocksdb/src/nativeTest/kotlin/util/testUtils.kt
+++ modules/rocksdb/src/nativeTest/kotlin/util/testUtils.kt
@@ -1,13 +1,29 @@
 package dev.adamko.kotlin.on.the.rocksdb.util
 
+import kotlinx.cinterop.*
+import platform.posix.CLOCK_REALTIME
+import platform.posix.clock_gettime
+import platform.posix.timespec
 import kotlin.random.Random
 import kotlin.random.nextUInt
-import kotlin.system.getTimeMillis
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
 
 internal fun tempDir(): String {
-  val dbName = "rocksdb_temp_${getTimeMillis()}${Random.nextUInt()}"
+  val dbName = "rocksdb_temp_${systemTimeMillis()}_${Random.nextUInt()}"
   return when (Platform.osFamily) {
     OsFamily.WINDOWS -> "C:\\Windows\\Temp\\$dbName"
-    else             -> "/tmp/$dbName"
+    else             -> "/tmp/$dbName/"
+  }
+}
+
+private fun systemTimeMillis(): Long {
+  return memScoped {
+    val timespec = alloc<timespec>()
+    clock_gettime(CLOCK_REALTIME.convert(), timespec.ptr)
+    @OptIn(UnsafeNumber::class)
+    timespec
+      .run { tv_sec.seconds + tv_nsec.convert<Long>().nanoseconds }
+      .inWholeMilliseconds
   }
 }
