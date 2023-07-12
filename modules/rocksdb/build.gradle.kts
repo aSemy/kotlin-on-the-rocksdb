559fdb67e1c721b5c5459a1d714ca63647ed2a12
Adam <897017+aSemy@users.noreply.github.com>
2023-07-12 12:37:12 +0200

    add ExperimentalForeignApi opt-in

--- modules/rocksdb/build.gradle.kts
+++ modules/rocksdb/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
   sourceSets {
     configureEach {
       languageSettings.optIn("kotlin.ExperimentalStdlibApi")
+      languageSettings.optIn("kotlinx.cinterop.ExperimentalForeignApi")
       languageSettings.optIn("dev.adamko.kotlin.on.the.rocksdb.KotlinOnTheRocksDbInternalApi")
     }
 
