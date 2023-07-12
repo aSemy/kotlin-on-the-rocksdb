055ab0de3df01d904b97fbd7c52ea7057142d6c0
Adam <897017+aSemy@users.noreply.github.com>
2023-07-12 12:14:06 +0200

    add checkWorkflowRocksDBVersion task

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,3 +19,29 @@ idea {
     )
   }
 }
+
+
+val checkWorkflowRocksDBVersion by tasks.registering {
+  group = LifecycleBasePlugin.VERIFICATION_GROUP
+
+  val workflowDir = layout.projectDirectory.dir(".github/workflows/")
+  inputs.dir(workflowDir).withPropertyName("workflowDir")
+
+  val rdbVersion = libs.versions.rocksDb
+  inputs.property("rdbVersion", rdbVersion)
+
+  doLast {
+    workflowDir.asFileTree.files.forEach { workflow ->
+      val rocksDbVersionLines = workflow.readLines()
+        .filter { !it.trim().startsWith("#") && it.contains("ROCKSDB_VERSION") }
+
+      if (rocksDbVersionLines.isNotEmpty() && rocksDbVersionLines.none { line -> rdbVersion.get() in line }) {
+        error("$workflow does not have latest RocksDB Version of ${rdbVersion.get()}")
+      }
+    }
+  }
+}
+
+tasks.check {
+  dependsOn(checkWorkflowRocksDBVersion)
+}
